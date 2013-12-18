0b1cc05723a486487615d1614d4364654c4e1817
estnpas <steven.parks@ericsson.com>
2013-12-18 10:02:03 -0800

    New helper routines and new table management model

--- workspace/SkillSet/src/com/ericsson/model/db/BaseDBModel.java
+++ workspace/SkillSet/src/com/ericsson/model/db/BaseDBModel.java
@@ -8,12 +8,40 @@ import org.apache.commons.lang3.time.DateFormatUtils;
 public abstract class BaseDBModel 
 	implements Serializable {
 	
+	public static final String COL_ID = "Id";
+	public static final String COL_RESOURCEID = "ResourceId";
 	public static final String COL_ATTRIBUTEVALUEID = "AttributeValueId";
 	public static final String COL_ATTRIBUTEID = "AttributeId";
 	public static final String COL_ATTRIBUTEVALUE = "AttributeValue";
 	public static final String COL_LASTUPDATED = "Last_Updated";
 	public static final String COL_ISDELETED = "Is_Deleted";
 	public static final String COL_MODIFIEDBY = "ModifiedBy";
+	
+	private Date lastUpdated;
+	private boolean deleted=false;
+	private int modifiedBy;
+	
+	public Date getLastUpdated() {
+		return lastUpdated;
+	}
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+	
+	public boolean isDeleted() {
+		return deleted;
+	}
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+	
+	public void setModifiedBy(int modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	
+	public int getModifiedBy() {
+		return modifiedBy;
+	}
 	
 	protected String convertToDate(Date date) {
 		return DateFormatUtils.format(date, "dd-MMM-yyyy HH:mm:ss");
@@ -23,6 +51,87 @@ public abstract class BaseDBModel
 		return (value) ? 1 : 0;
 	}
 	
+	protected int convertToBit(Boolean value) {
+		return (value.booleanValue()) ? 1 : 0;
+	}
+	
+	protected String convertField(Object value) {
+		String sqlValue = "";
+		if (value==null) {
+			sqlValue = "NULL";
+		} else if (value instanceof Date) {
+			sqlValue = convertToDate((Date)value);
+		} else if (value instanceof Boolean) {
+			sqlValue = String.valueOf(convertToBit((Boolean)value));
+		}
+		return sqlValue;
+	}
+	
 	public abstract String getTableName();
 	public abstract String generateInsertSQL();
+	
+	protected void startInsertSQL(StringBuffer buffer) {
+		buffer.append("INSERT INTO ");
+		buffer.append(getTableName());
+	}
+	
+	protected void startColumnsClause(StringBuffer buffer) {
+		buffer.append("(");
+	}
+	
+	protected void endColumnsClause(StringBuffer buffer) {
+		buffer.append(")");
+	}
+	
+	protected void addColumn(
+						StringBuffer buffer, 
+						String columnName,
+						boolean appendComma) {
+		buffer.append(columnName);
+		if (appendComma) {
+			buffer.append(",");
+		}
+	}
+	
+	protected void addColumn(
+						StringBuffer buffer, 
+						String columnName) {
+		addColumn(buffer, columnName, false);
+	}
+	
+	protected void addDefaultColumns(StringBuffer buffer) {
+		addColumn(buffer, COL_LASTUPDATED, true);
+		addColumn(buffer, COL_ISDELETED, true);
+		addColumn(buffer, COL_MODIFIEDBY);
+	}
+	
+	protected void startValuesClause(StringBuffer buffer) {
+		buffer.append(" VALUES (");
+	}
+	
+	protected void endValuesClause(StringBuffer buffer) {
+		buffer.append(")");
+	}
+	
+	protected void addField(
+						StringBuffer buffer, 
+						Object value, 
+						boolean appendComma) {
+		buffer.append(convertField(value));
+		if (appendComma) {
+			buffer.append(",");
+		}
+	}
+	
+	protected void addField(
+						StringBuffer buffer,
+						Object value) {
+		addField(buffer, value, false);
+	}
+	
+	protected void addDefaultFields(StringBuffer buffer) {
+		addField(buffer, getLastUpdated(), true);
+		addField(buffer, isDeleted(), true);
+		addField(buffer, getModifiedBy());
+	}
 }
---
+++ workspace/SkillSet/src/com/ericsson/model/db/ResourceMasterAttributes.java
@@ -0,0 +1,66 @@
+package com.ericsson.model.db;
+
+public class ResourceMasterAttributes 
+	extends BaseDBModel {
+	
+	public static final String TABLE_NAME = "ResourceMasterAttributes";
+	
+	private int id;
+	private int resourceId;
+	private int attributeId;
+	private int attributeValueId;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getResourceId() {
+		return resourceId;
+	}
+	public void setResourceId(int resourceId) {
+		this.resourceId = resourceId;
+	}
+	public int getAttributeId() {
+		return attributeId;
+	}
+	public void setAttributeId(int attributeId) {
+		this.attributeId = attributeId;
+	}
+	public int getAttributeValueId() {
+		return attributeValueId;
+	}
+	public void setAttributeValueId(int attributeValueId) {
+		this.attributeValueId = attributeValueId;
+	}
+	
+	public String generateInsertSQL() {
+		StringBuffer buffer = new StringBuffer();
+		
+		startInsertSQL(buffer);
+		
+		startColumnsClause(buffer);
+		addColumn(buffer, COL_ID, true);
+		addColumn(buffer, COL_RESOURCEID, true);
+		addColumn(buffer, COL_ATTRIBUTEID, true);
+		addColumn(buffer, COL_ATTRIBUTEVALUEID, true);
+		addDefaultColumns(buffer);
+		endColumnsClause(buffer);
+		
+		startValuesClause(buffer);
+		addField(buffer, getId(), true);
+		addField(buffer, getResourceId(), true);
+		addField(buffer, getAttributeId(), true);
+		addField(buffer, getAttributeValueId(), true);
+		addDefaultFields(buffer);
+		endValuesClause(buffer);
+		
+		return buffer.toString();
+	}
+	
+	public String getTableName() {
+		return TABLE_NAME;
+	}
+
+}
